547d946a893ec016b51fe05a9fa6da3f56196734
Ivan Naluzhnyi <ivan.naluzhnyi@gmail.com>
2019-05-31 12:56:02 +0200

    initial commit + add main file

---
+++ RDVcalendar.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+Plugin Name: RDVcalendar
+Plugin URI:http://wordpress.org/plugins/RDVcalendar
+Description: TO DO
+Author: Ivan Naluzhnyi / Mikael PAUL / Theo LEGAGNEUR
+Version: 0.1.0
+Author URI: http://RDVcalendar.com
+*/
+
+// TO DO
+
