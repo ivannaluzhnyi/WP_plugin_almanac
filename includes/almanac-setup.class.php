f72a1c42d2d157a32117946ae11ff01bfc4114cc
Ivan Naluzhnyi <ivan.naluzhnyi@gmail.com>
2019-07-01 11:42:16 +0200

    add event column

--- includes/almanac-setup.class.php
+++ includes/almanac-setup.class.php
@@ -22,11 +22,11 @@ class almanac_events
 
         add_action("wp_enqueue_scripts", array(&$this, 'front_js_libs'));
 
-        add_action( 'init', array( &$this, 'add_custom_post_type' ) );
+        add_action('init', array(&$this, 'add_custom_post_type'));
 
-        // add_filter('manage_edit-events_columns', array( &$this, 'add_new_events_columns' ));
+        add_filter('manage_edit-events_columns', array( &$this, 'add_new_events_columns' ));
 
-        // add_action('manage_events_posts_custom_column', array( &$this, 'manage_events_columns' ), 10, 2);
+        add_action('manage_events_posts_custom_column', array( &$this, 'manage_events_columns' ), 10, 2);
 
         // add_filter('pre_get_posts', array( &$this, 'show_events_for_current_user_only' ));
 
@@ -45,6 +45,8 @@ class almanac_events
         // add_action('wp_ajax_nopriv_my_special_action', array( &$this, 'my_action_callback') ); 
 
         // add_action('wp_ajax_my_special_action', array( &$this, 'my_action_callback') ); 
+
+
     }
 
 
@@ -100,50 +102,85 @@ class almanac_events
     }
 
 
-    function add_custom_post_type() {
+    function add_custom_post_type()
+    {
+
+        $labels = array(
+            'name' => _x('Événements', 'post type general name'),
+            'singular_name' => _x('Événement', 'post type singular name'),
+            'add_new' => _x('Ajouter un nouvel événement', 'Testimonial'),
+            'add_new_item' => __('Ajouter un nouvel événement'),
+            'edit_item' => __('Modifier l\'événement'),
+            'new_item' => __('Nouvel événement'),
+            'view_item' => __('Voir événement'),
+            'search_items' => __('Trouver  événement'),
+            'not_found' =>  __('Rien n\'a été trouvé'),
+            'not_found_in_trash' => __('Rien trouvé dans la corbeille'),
+            'parent_item_colon' => ''
+        );
+
+        $args = array(
+            'labels' => $labels,
+            'public' => true,
+            'publicly_queryable' => true,
+            'show_ui' => true,
+            'show_in_menu' => true,
+            'query_var' => true,
+            'menu_icon' => WPE_url . '/img/events.png',
+            'rewrite' => true,
+            //'map_meta_cap' => true,
+            'capability_type' => 'calendar',
+            'capabilities' => array(
+                'publish_posts' => 'publish_calendars',
+                'edit_posts' => 'edit_calendars',
+                'edit_others_posts' => 'edit_others_calendars',
+                'delete_posts' => 'delete_calendars',
+                'delete_others_posts' => 'delete_others_calendars',
+                'read_private_posts' => 'read_private_calendars',
+                'edit_post' => 'edit_calendar',
+                'delete_post' => 'delete_calendar',
+                'read_post' => 'read_calendar',
+            ),
+            'has_archive' => true,
+            'hierarchical' => false,
+            'menu_position' => null,
+            'supports' => array('title', 'editor', 'thumbnail')
+        );
+
+        register_post_type('events', $args);
+    }
+
+
+
+    function add_new_events_columns($events_columns) {
 	
-		$labels = array(
-			'name' => _x('Events', 'post type general name'),
-			'singular_name' => _x('Event', 'post type singular name'),
-			'add_new' => _x('Add New Event', 'Testimonial'),
-			'add_new_item' => __('Add New Event'),
-			'edit_item' => __('Edit Event'),
-			'new_item' => __('New Event'),
-			'view_item' => __('View Event'),
-			'search_items' => __('Search Event'),
-			'not_found' =>  __('Nothing found'),
-			'not_found_in_trash' => __('Nothing found in Trash'),
-			'parent_item_colon' => ''
-		);
+		$new_columns['cb'] = '<input type="checkbox" />';
+ 
+		$new_columns['date_time'] = 'Date d\'événement';
 		
-		$args = array(
-			'labels' => $labels,
-			'public' => true,
-			'publicly_queryable' => true,
-			'show_ui' => true, 
-			'show_in_menu' => true, 
-			'query_var' => true,
-			'menu_icon' => WPE_url . '/img/events.png',
-			'rewrite' => true,
-			//'map_meta_cap' => true,
-			'capability_type' => 'calendar',
-			'capabilities' => array(
-				'publish_posts' => 'publish_calendars',
-				'edit_posts' => 'edit_calendars',
-				'edit_others_posts' => 'edit_others_calendars',
-				'delete_posts' => 'delete_calendars',
-				'delete_others_posts' => 'delete_others_calendars',
-				'read_private_posts' => 'read_private_calendars',
-				'edit_post' => 'edit_calendar',
-				'delete_post' => 'delete_calendar',
-				'read_post' => 'read_calendar',
-			),
-			'has_archive' => true, 
-			'hierarchical' => false,
-			'menu_position' => null,
-			'supports' => array('title', 'editor', 'thumbnail')
-		); 
+		$new_columns['title'] = 'Nom d\'événement ';
+ 
+		return $new_columns;
 		
-		register_post_type('events',$args);
 	}
+	
+	function manage_events_columns($column_name, $id) {
+	
+		global $wpdb;
+		switch ($column_name) {
+		case 'date_time':
+			$events_date_meta = get_post_meta($id,'events_date',true);
+			if($events_date_meta == ''){
+				$events_date = '';
+				$events_time = '';
+			}else{
+				$events_date = date('jS \o\f F Y', $events_date_meta);
+				$events_time = date('g.ia', $events_date_meta);
+			}
+			echo $events_date . '<br>' . $events_time; 
+		        break;
+		default:
+			break;
+		} // end switch
+	}	
 }
